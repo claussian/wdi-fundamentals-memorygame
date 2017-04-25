eb516699a191f0025651cc185747796b2232a703
Hanif Samad <hanif.samad.sg@gmail.com>
2017-04-25 20:08:10 +0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -2,21 +2,24 @@ console.log("Up and running!");
 
 var cards = ['queen','queen','king','king'];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
 
-console.log("User flipped " + cardsInPlay[cardsInPlay.length - 1]);
-
-var cardTwo = cards[3];
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardsInPlay[cardsInPlay.length - 1]);
-
-if(cardsInPlay.length === 2){
-	if(cardsInPlay[0] === cardsInPlay[1]){
-		alert("You found a match!");
-	}
-	else{
-		alert("Sorry, try again.");
-	}
+//Function to check for match
+var checkForMatch = function(){
+	if(cardsInPlay.length === 2){
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match!");
+		}
+		else {
+			alert("Sorry, try again.");
+		}
+	}	
 }
+//Function to execute after user flips a card
+var flipCard = function(cardId){
+	console.log("User flipped " + cards[cardId])
+	cardsInPlay.push(cards[cardId])
+	checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
